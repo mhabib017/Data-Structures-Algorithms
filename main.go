aa1150244c7acf23a96c2e81eefadd9dd0ccc400
Muhammad Habib <habibchk123@gmail.com>
2022-04-11 17:16:02 +0500

    implemented linked-list

--- main.go
+++ main.go
@@ -3,48 +3,68 @@ package main
 import "fmt"
 
 type Node struct {
-	key int
-	left *Node
-	right *Node
+	data int
+	next *Node
 }
 
-func (n *Node) insert(val int) {
-	
-	if n.key < val {
-		if n.right == nil {
-			n.right = &Node{key: val}
-		}else {
-			n.right.insert(val)
+type LinkedList struct {
+	head   *Node
+	length int
+}
+
+func (l *LinkedList) prepand(v int) {
+	node := Node{data: v}
+	node.next = l.head
+	l.head = &node
+	l.length++
+}
+
+func (l *LinkedList) printList(){
+	node:=l.head
+	length := l.length
+	for length !=0 {
+		fmt.Print(node.data, " ")
+		node = node.next
+		length--
+	}
+	fmt.Println()
+}
+
+func (l *LinkedList) delete(val int) {
+	if l.length ==0{
+		return 
+	}
+	if l.head.data == val {
+		l.head = l.head.next
+		l.length--
+		return
+	}
+	previousToDelete := l.head
+	for previousToDelete.next.data != val {
+		if previousToDelete.next.next == nil {
+			return
 		}
-	}else if n.key > val {
-		if n.left == nil {
-			n.left =  &Node{key: val}
-		}else {
-			n.left.insert(val)
-		}
+		previousToDelete = previousToDelete.next
 	}
+	previousToDelete.next = previousToDelete.next.next
+	l.length--
 }
 
-func (n *Node) search(val int) bool {
-	if n==nil {
-		return false
-	}
-	if n.key < val {
-		return n.right.search(val)
-	}else if n.key >val {
-		return n.left.search(val)
-	}
-	return true
-}
-
-func  main()  {
-	tree:= &Node{key: 50}
-	tree.insert(20)
-	tree.insert(40)
-	tree.insert(10)
-	tree.insert(11)
-	fmt.Println(tree.search(60))
-	fmt.Println(tree.search(40))
-	fmt.Println(tree.search(110))
-	fmt.Println(tree.search(11))
+func main() {
+	linkedList := LinkedList{}
+	linkedList.prepand(10)
+	linkedList.prepand(20)
+	linkedList.prepand(30)
+	linkedList.prepand(40)
+	linkedList.prepand(50)
+	linkedList.prepand(60)
+	linkedList.prepand(70)
+	linkedList.prepand(80)
+	linkedList.printList()
+	linkedList.delete(60)
+	linkedList.delete(70)
+	linkedList.delete(80)
+	linkedList.delete(10)
+	linkedList.delete(20)
+	linkedList.printList()
 }
