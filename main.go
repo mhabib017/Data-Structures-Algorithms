b1c42485445cca6c949637fb2eeb51b64aebb425
Muhammad Habib <habibchk123@gmail.com>
2022-04-11 17:07:02 +0500

    implemented stack

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	fmt.Println("ok running!")
+type stack struct {
+	items []int
+}
+
+func (s *stack) push(a int){
+	s.items = append(s.items, a)
+}
+func (s *stack) pop() int {
+	l := len(s.items)-1
+	val := s.items[l]
+	s.items = s.items[:l]
+	return val
+}
+
+func main(){
+	myStack := stack{}
+	myStack.push(10)
+	myStack.push(20)
+	myStack.push(30)
+	fmt.Println(myStack)
+	fmt.Println(myStack.pop())
+	fmt.Println(myStack)
 }
