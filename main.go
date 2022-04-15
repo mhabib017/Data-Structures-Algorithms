f4fc333148ee2d4a1cac8cbe9337cc1d4c2cd681
Muhammad Habib <habibchk123@gmail.com>
2022-04-15 11:19:21 +0500

    implemented hashtable

--- main.go
+++ main.go
@@ -1,43 +1,100 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func mergeSort(items []int) []int {
-	if len(items) < 2 {
-		return items
-	}
-	first := mergeSort(items[:len(items)/2])
-	second := mergeSort(items[len(items)/2:])
-	return merge(first, second)
+const ARRAY_SIZE = 5
+
+type HashTable struct {
+	array [ARRAY_SIZE]*Bucket
 }
 
-func merge(a []int, b []int) []int {
-	final := []int{}
-	i := 0
-	j := 0
+func (h *HashTable) insert(key string){
+	index:= hash(key)
+	h.array[index].insert(key)
+}
 
-	for i < len(a) && j < len(b) {
-		if a[i] < b[j] {
-			final = append(final, a[i])
-			i++
-		} else {
-			final = append(final, b[j])
-			j++
+func (h HashTable) search(key string)bool {
+	index:= hash(key)
+	return h.array[index].search(key)
+}
+
+func (h *HashTable) delete(key string)  {
+	index:= hash(key)
+	h.array[index].delete(key)
+}
+
+
+type Bucket struct {
+	head *BucketNode
+}
+
+type BucketNode struct{
+	key string
+	next *BucketNode
+}
+
+func (b *Bucket) insert(key string){
+	
+	newNode := BucketNode{key:key}
+	newNode.next = b.head
+	b.head = &newNode
+	
+}
+
+func (b *Bucket) delete(key string) {
+	if b.head.key == key {
+		b.head = b.head.next
+		return
+	}
+	previousToDelete := b.head
+	for previousToDelete.next != nil {
+		if previousToDelete.next.key == key{
+			previousToDelete.next = previousToDelete.next.next
+			return
 		}
+		previousToDelete = previousToDelete.next
 	}
+}
 
-	for ; i < len(a); i++ {
-		final = append(final, a[i])
+func (b Bucket) search(key string) bool{
+	current:= b.head
+	for current != nil {
+		if current.key == key {
+			return true
+		}
+		current = current.next
 	}
-	for ; j < len(b); j++ {
-		final = append(final, b[j])
+	return false
+}
+
+
+func initHashTable() *HashTable{
+	hashTable:= HashTable{}
+	for i:= range  hashTable.array{
+		hashTable.array[i] = &Bucket{}
 	}
-	return final
+	return &hashTable
+}
+
+func hash(key string) int{
+	sum:=0
+	for i:= range key{
+		sum+= int(i)
+	}
+	return sum % ARRAY_SIZE
 }
 
 func main() {
-	unsorted := []int{10, 6, 2, 1, 5, 8, 3, 4, 7, 9}
-	fmt.Println(unsorted)
-	sorted := mergeSort(unsorted)
-	fmt.Println(sorted)
+	hashTable := initHashTable()
+
+	hashTable.insert("Randy")
+	hashTable.insert("Randy")
+	hashTable.insert("Eric")
+	fmt.Println(hashTable.search("Randy"))
+	hashTable.delete("Randy")
+	fmt.Println(hashTable.search("Randy"))
+	fmt.Println(hashTable.search("Ben"))
+
 }
